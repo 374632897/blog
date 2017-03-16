10228b6e248216b6a7af683105e4d9199a873dd8
Jason <374632897@qq.com>
2017-03-16 10:56:36 +0800

    add

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 cp ~/{.aliases,.functions}.sh ~/.bash_profile .;
-git add .
-git commit -m 'auto-copy from the home directory';
+git commit -am 'auto-copy from the home directory';
 git push
